382cf4eb447dbb2c06da2dbf5badda3119386b00
ZIV LEVY <zivilevy@gmail.com>
2018-10-20 18:38:28 +0300

    angular way

--- src/app/govmap-placing/govmap-placing.component.ts
+++ src/app/govmap-placing/govmap-placing.component.ts
@@ -1,160 +1,131 @@
-import {AfterContentInit, AfterViewInit, Component, Input, OnChanges, OnInit} from '@angular/core';
+import {AfterContentInit, AfterViewInit, Component, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-govmap-placing',
-  templateUrl: './govmap-placing.component.html',
-  styleUrls: ['./govmap-placing.component.scss']
+    selector: 'app-govmap-placing',
+    templateUrl: './govmap-placing.component.html',
+    styleUrls: ['./govmap-placing.component.scss']
 })
 export class GovmapPlacingComponent implements OnInit, AfterViewInit, OnChanges {
+@ViewChild('pin') pin;
 
+    constructor() {
 
-  constructor() {
-
-  }
+    }
 
     ngAfterViewInit() {
     }
 
-    ngOnChanges (change) {
+    ngOnChanges(change) {
     }
 
 
-  ngOnInit() {
-    var DocumentReady = function(){
-    govmap.createMap('map', {
+    ngOnInit() {
 
-        token: "1f3f77a5-064f-46f6-941e-f9eb8a3c09b2",
-        showXY: true,
-        bgButton: 0,
-        zoomButtons: 0,
-        identifyOnClick: 0,
-        layersMode: 4,
-        onLoad: startMap
-    });
+        window['govmap'].createMap('map', {
 
-    function startMap() {
-        var options = {
+            token: '1f3f77a5-064f-46f6-941e-f9eb8a3c09b2',
+            showXY: true,
+            bgButton: 1,
+            zoomButtons: 1,
+            identifyOnClick: 1,
+            layersMode: 4,
+            onLoad: this.startMap.bind(this)
+        });
+
+
+    }
+
+    startMap() {
+        const options = {
             enableHighAccuracy: true,
             timeout: 50000000,
             maximumAge: 0
         };
 
-        ///get first location
-        function success(pos) {
-            var crd = pos.coords;
-            var israelCoords = WgsToIsrael(crd.latitude, crd.longitude);
-            var params = {
+
+        navigator.geolocation.getCurrentPosition((pos) => {
+            const crd = pos.coords;
+            const israelCoords = this.WgsToIsrael(crd.latitude, crd.longitude);
+            const params = {
                 x: israelCoords[0],
                 y: israelCoords[1],
                 level: 10
             };
-            govmap.zoomToXY(params);
-            govmap.setBackground(2);
-            document.getElementById("pin").classList.remove('hide');
-        }
-
-        function error(err) {
+            window['govmap'].zoomToXY(params);
+            window['govmap'].setBackground(2);
+            // document.getElementById('pin').classList.remove('hide');
+        }, (err) => {
             console.warn(`ERROR(${err.code}): ${err.message}`);
-        }
+        }, options);
+        window['govmap'].setBackground(2);
 
-        navigator.geolocation.getCurrentPosition(success, error, options);
-        govmap.setBackground(2);
     }
-};
-
-if (
-    document.readyState === "complete" ||
-    (document.readyState !== "loading" && !document.documentElement.doScroll)
-) {
-  DocumentReady();
-} else {
-  document.addEventListener("DOMContentLoaded", DocumentReady);
-}
 
 
 //////////////////////////////////////////
 // convert wgs84 to israel tm grid...
 //////////////////////////////////////////
 
-function degreesToRadians(degrees) {
-    return degrees * Math.PI / 180;
-}
 
-function pow2(x) {
-    return x * x;
-}
-
-function pow3(x) {
-    return x * x * x;
-}
-
-function pow4(x) {
-    return x * x * x * x;
-}
-
-function ITMLocation(easting, northing) {
-    this.easting = easting;
-    this.northing = northing;
-    this.eastingInOldGrid = function() {
-        return easting - 50000;
+    degreesToRadians(degrees) {
+        return degrees * Math.PI / 180;
     }
-    this.northingInOldGrid = function() {
-        var retval = northing - 500000;
-        if (retval < 0) {
-            retval += 1000000;
-        }
-        return retval;
+
+
+    pow2(x) {
+        return x * x;
     }
-}
 
-//////////////
-//// Main Function…
-//////////////
-function WgsToIsrael(latitude, longitude) {
-    longitude = degreesToRadians(longitude);
-    latitude = degreesToRadians(latitude);
-    //LatLongToITM(latitude, longitud);
-    // Projection parameters
-    var centralMeridian = degreesToRadians(35.2045169444444); // central meridian of ITM projection
-    var k0 = 1.0000067; // scale factor
 
-    // Ellipsoid constants (WGS 80 datum)
-    var a = 6378137; // equatorial radius
-    var b = 6356752.3141; // polar radius
-    var e = Math.sqrt(1 - b * b / a / a); // eccentricity
-    var e1sq = e * e / (1 - e * e);
-    var n = (a - b) / (a + b);
+    pow3(x) {
+        return x * x * x;
+    }
 
-    // Curvature at specified location
-    var tmp = e * Math.sin(latitude);
-    var nu = a / Math.sqrt(1 - tmp * tmp);
 
-    // Meridional arc length
-    var n3 = pow3(n);
-    var n4 = pow4(n);
-    var A0 = a * (1 - n + (5 * n * n / 4) * (1 - n) + (81 * n4 / 64) * (1 - n));
-    var B0 = (3 * a * n / 2) * (1 - n - (7 * n * n / 8) * (1 - n) + 55 * n4 / 64);
-    var C0 = (15 * a * n * n / 16) * (1 - n + (3 * n * n / 4) * (1 - n));
-    var D0 = (35 * a * n3 / 48) * (1 - n + 11 * n * n / 16);
-    var E0 = (315 * a * n4 / 51) * (1 - n);
-    var S = A0 * latitude - B0 * Math.sin(2 * latitude) + C0 * Math.sin(4 * latitude) - D0 * Math.sin(6 * latitude) + E0 * Math.sin(8 * latitude);
+    pow4(x) {
+        return x * x * x * x;
+    }
 
-    // Coefficients for ITM coordinates
-    var p = longitude - centralMeridian;
-    var Ki = S * k0;
-    var Kii = nu * Math.sin(latitude) * Math.cos(latitude) * k0 / 2;
-    var Kiii = ((nu * Math.sin(latitude) * pow3(Math.cos(latitude))) / 24) * (5 - pow2(Math.tan(latitude)) + 9 * e1sq * pow2(Math.cos(latitude)) + 4 * e1sq * e1sq * pow4(Math.cos(latitude))) * k0;
-    var Kiv = nu * Math.cos(latitude) * k0;
-    var Kv = pow3(Math.cos(latitude)) * (nu / 6) * (1 - pow2(Math.tan(latitude)) + e1sq * pow2(Math.cos(latitude))) * k0;
 
-    var easting = Math.round(219529.58 + Kiv * p + Kv * pow3(p) - 60);
-    var northing = Math.round(Ki + Kii * p * p + Kiii * pow4(p) - 3512424.41 + 626907.39 - 45);
-    return [easting, northing];
-}
-///////////////////
-// end convert
-///////////////////
+    WgsToIsrael(latitude, longitude) {
+        longitude = this.degreesToRadians(longitude);
+        latitude = this.degreesToRadians(latitude);
+        // Projection parameters
+        const centralMeridian = this.degreesToRadians(35.2045169444444); // central meridian of ITM projection
+        const k0 = 1.0000067; // scale factor
 
-  }
+        // Ellipsoid constants (WGS 80 datum)
+        const a = 6378137; // equatorial radius
+        const b = 6356752.3141; // polar radius
+        const e = Math.sqrt(1 - b * b / a / a); // eccentricity
+        const e1sq = e * e / (1 - e * e);
+        const n = (a - b) / (a + b);
+
+        // Curvature at specified location
+        const tmp = e * Math.sin(latitude);
+        const nu = a / Math.sqrt(1 - tmp * tmp);
+
+        // Meridional arc length
+        const n3 = this.pow3(n);
+        const n4 = this.pow4(n);
+        const A0 = a * (1 - n + (5 * n * n / 4) * (1 - n) + (81 * n4 / 64) * (1 - n));
+        const B0 = (3 * a * n / 2) * (1 - n - (7 * n * n / 8) * (1 - n) + 55 * n4 / 64);
+        const C0 = (15 * a * n * n / 16) * (1 - n + (3 * n * n / 4) * (1 - n));
+        const D0 = (35 * a * n3 / 48) * (1 - n + 11 * n * n / 16);
+        const E0 = (315 * a * n4 / 51) * (1 - n);
+        const S = A0 * latitude - B0 * Math.sin(2 * latitude) + C0 * Math.sin(4 * latitude) - D0 * Math.sin(6 * latitude) + E0 * Math.sin(8 * latitude);
+
+        // Coefficients for ITM coordinates
+        const p = longitude - centralMeridian;
+        const Ki = S * k0;
+        const Kii = nu * Math.sin(latitude) * Math.cos(latitude) * k0 / 2;
+        const Kiii = ((nu * Math.sin(latitude) * this.pow3(Math.cos(latitude))) / 24) * (5 - this.pow2(Math.tan(latitude)) + 9 * e1sq * this.pow2(Math.cos(latitude)) + 4 * e1sq * e1sq * this.pow4(Math.cos(latitude))) * k0;
+        const Kiv = nu * Math.cos(latitude) * k0;
+        const Kv = this.pow3(Math.cos(latitude)) * (nu / 6) * (1 - this.pow2(Math.tan(latitude)) + e1sq * this.pow2(Math.cos(latitude))) * k0;
+
+        const easting = Math.round(219529.58 + Kiv * p + Kv * this.pow3(p) - 60);
+        const northing = Math.round(Ki + Kii * p * p + Kiii * this.pow4(p) - 3512424.41 + 626907.39 - 45);
+        return [easting, northing];
+    }
 
 }
